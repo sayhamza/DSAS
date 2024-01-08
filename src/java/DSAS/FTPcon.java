53e3cfd230393c5a216a3c648cdf76ec93482629
sayhamza <sayedhamza88882651@gmail.com>
2024-01-08 13:42:23 +0530

    commit done

---
+++ src/java/DSAS/FTPcon.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DSAS;
+
+/**
+ *
+ * @author java1
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FTPcon {
+
+    FTPClient client = new FTPClient();
+    FileInputStream fis = null;
+    boolean status;
+
+    /**
+     *
+     * @param file
+     * @return
+     */
+    public boolean upload(File file) {
+        try {
+            System.out.println("Check------------------------------------->1");
+            client.connect("ftp.drivehq.com");
+            client.login("ENTER YOUR USERNAME", "ENTER YOUR PASSWORD");
+            client.enterLocalPassiveMode();
+            fis = new FileInputStream(file);
+            System.out.println("Check------------------------------------->2");
+            status = client.storeFile("/" + file.getName(), fis);
+            //file path of drive storage
+            client.logout();
+            fis.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        if (status) {
+            System.out.println("success");
+            return true;
+        } else {
+            System.out.println("failed");
+            return false;
+
+        }
+
+    }
+}
